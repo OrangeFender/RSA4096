8ff791403969461e23af712fcc970a6af90d7fd4
Yunfan Zhang <zyfdmap@126.com>
2023-10-13 22:16:18 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,8 +2,8 @@
 #include"Multiplication.h"
 
 int main(){
-    uInt2048 a("123456789abcdef");
-    uInt2048 b("123456789abcdef");
+    uInt2048 a("123456789abcdef123456789");
+    uInt2048 b("123456789abcdef123456789");
     a.printHEX();
     b.printHEX();
     uInt2048 c=a*b;
